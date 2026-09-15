5a134c84c1a9be0a89511e23ad81015de2011d1b
sidneysx <skylerbr3@gmail.com>
2026-09-15 13:48:57 -0300

    Funçoes e parametros opcionais

--- index.php
+++ index.php
@@ -1,7 +1,48 @@
 <?php
 
-function saudacao(){
-    return "Olá, seja bem-vindo(a)!";
+// function saudacao(){
+//     return "Olá, seja bem-vindo(a)!";
+// }
+
+// echo saudacao();
+
+function verificarIdade($anoNascimento)
+{
+    $anoAtual = date("Y");
+    $idade = $anoAtual - $anoNascimento;
+    return  $idade;
+
 }
 
-echo saudacao();
+function portaoDeEntrada($anoNascimento, $acompanhante = false)
+{
+    $idadeDeEntraNaBalada = 18;
+    $idade = verificarIdade($anoNascimento);
+
+    if ($idade >= $idadeDeEntraNaBalada) {
+        return "Pode entrar na balada!";
+    } elseif ($idade < $idadeDeEntraNaBalada && $acompanhante) {
+        return "Pode entrar na balada com seu acompanhante!";
+    } else {
+        return "Voce nao pode entrar na balada!";
+    }
+}
+
+
+
+// Cenário 1
+$anoNascimento = 2005;
+$idade = verificarIdade($anoNascimento);
+echo "'$idade' anos - " . portaoDeEntrada($anoNascimento, true) . "\n";
+echo "<br>";
+
+// Cenário 2
+$anoNascimento = 2020;
+$idade = verificarIdade($anoNascimento);
+echo "'$idade' anos - " . portaoDeEntrada($anoNascimento, true) . "\n";
+echo "<br>";
+
+// Cenário 3
+$anoNascimento = 2020;
+$idade = verificarIdade($anoNascimento);
+echo "'$idade' anos - " . portaoDeEntrada($anoNascimento, false) . "\n";
